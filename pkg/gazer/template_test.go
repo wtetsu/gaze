b5424d2c300ea949e92e9699dad68b5a2f7a0549
wtetsu <tewatana@gmail.com>
2020-01-28 00:16:36 +0900

    Add some tests

---
+++ pkg/gazer/template_test.go
@@ -0,0 +1,22 @@
+/**
+ * Gaze (https://github.com/wtetsu/gaze/)
+ * Copyright 2020-present wtetsu
+ * Licensed under MIT
+ */
+
+package gazer
+
+import (
+	"testing"
+)
+
+func TestTemplate1(t *testing.T) {
+	r, err := render("{{file}} {{ext}} {{base}} {{dir}} {{base0}} {{base1}} {{base2}}", "/full/path/test.txt.bak")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r != "/full/path/test.txt.bak .bak test.txt.bak /full/path test test.txt test.txt.bak" {
+		t.Fatal(r)
+	}
+}
--- pkg/notify/notify_test.go
+++ pkg/notify/notify_test.go
@@ -10,13 +10,14 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/wtetsu/gaze/pkg/logger"
 	"github.com/wtetsu/gaze/pkg/time"
 )
 
-func TestBasic(t *testing.T) {
+func TestUpdate(t *testing.T) {
 	logger.Level(logger.VERBOSE)
 
 	rb := createTempFile("*.rb", `puts "Hello from Ruby`)
@@ -26,7 +27,7 @@ func TestBasic(t *testing.T) {
 		t.Fatal("Temp files error")
 	}
 
-	pattens := []string{rb, py}
+	pattens := []string{filepath.Dir(rb) + "/*.rb", filepath.Dir(rb) + "/*.py"}
 
 	notify, err := New(pattens)
 	if err != nil {
@@ -64,6 +65,133 @@ func TestBasic(t *testing.T) {
 		time.Sleep(20)
 	}
 	if count < 4 {
+		t.Fatalf("count:%d", count)
+	}
+
+	notify.Close()
+	notify.Close()
+}
+
+func TestCreateAndMove(t *testing.T) {
+	logger.Level(logger.VERBOSE)
+
+	rb1 := createTempFile("*.tmp", `puts "Hello from Ruby`)
+	rb2 := createTempFile("*.tmp", `puts "Hello from Ruby`)
+	py1 := createTempFile("*.tmp", `print("Hello from Python")`)
+	py2 := createTempFile("*.tmp", `print("Hello from Python")`)
+
+	if rb1 == "" || rb2 == "" || py1 == "" || py2 == "" {
+		t.Fatal("Temp files error")
+	}
+
+	pattens := []string{
+		filepath.Dir(rb1) + "/*.rb",
+		filepath.Dir(rb2) + "/*.rb",
+		filepath.Dir(py1) + "/*.py",
+		filepath.Dir(py2) + "/*.py",
+	}
+
+	notify, err := New(pattens)
+	if err != nil {
+		t.Fatal()
+	}
+
+	notify.PendingPeriod(10)
+
+	count := 0
+	go func() {
+		for {
+			select {
+			case _, ok := <-notify.Events:
+				if !ok {
+					continue
+				}
+				count++
+
+			case err, ok := <-notify.Errors:
+				if !ok {
+					continue
+				}
+				log.Println("error:", err)
+				count++
+			}
+		}
+	}()
+
+	os.Rename(rb1, rb1+".rb")
+	os.Rename(rb2, rb2+".rb")
+	os.Rename(py1, py1+".py")
+	os.Rename(py2, py2+".py")
+
+	for i := 0; i < 50; i++ {
+		if count >= 4 {
+			break
+		}
+		time.Sleep(20)
+	}
+
+	if count < 4 {
+		t.Fatalf("count:%d", count)
+	}
+
+	notify.Close()
+	notify.Close()
+}
+
+func TestDelete(t *testing.T) {
+	logger.Level(logger.VERBOSE)
+
+	rb1 := createTempFile("*.rb", `puts "Hello from Ruby`)
+	rb2 := createTempFile("*.rb", `puts "Hello from Ruby`)
+	py1 := createTempFile("*.py", `print("Hello from Python")`)
+	py2 := createTempFile("*.py", `print("Hello from Python")`)
+
+	if rb1 == "" || rb2 == "" || py1 == "" || py2 == "" {
+		t.Fatal("Temp files error")
+	}
+
+	pattens := []string{
+		filepath.Dir(rb1) + "/*.rb",
+		filepath.Dir(rb2) + "/*.rb",
+		filepath.Dir(py1) + "/*.py",
+		filepath.Dir(py2) + "/*.py",
+	}
+
+	notify, err := New(pattens)
+	if err != nil {
+		t.Fatal()
+	}
+
+	notify.PendingPeriod(10)
+
+	count := 0
+	go func() {
+		for {
+			select {
+			case _, ok := <-notify.Events:
+				if !ok {
+					continue
+				}
+				count++
+
+			case err, ok := <-notify.Errors:
+				if !ok {
+					continue
+				}
+				log.Println("error:", err)
+				count++
+			}
+		}
+	}()
+
+	os.Remove(rb1)
+	os.Remove(rb2)
+	os.Remove(py1)
+	os.Remove(py2)
+
+	time.Sleep(20)
+
+	if count != 0 {
 		t.Fatalf("count:%d", count)
 	}
 
